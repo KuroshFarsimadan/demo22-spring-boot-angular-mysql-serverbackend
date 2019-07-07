511bd1b2fc189c0f6a1f9940191d1fdcc63e1d90
Kurosh Farsimadan <kurosh_farsimadan@yahoo.com>
2019-07-06 20:24:34 -0400

    Add files via upload

---
+++ demo22-spring-boot-angular-mysql-serverbackend/src/main/java/ca/kuroshfarsimadan/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package ca.kuroshfarsimadan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ca.kuroshfarsimadan.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
